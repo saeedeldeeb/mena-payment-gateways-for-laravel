e1f2fc3ce99c5aad0a15518b5db85b9ebe180c6d
Saeed Eldeeb <saeedeldeeb1@gmail.com>
2023-04-19 13:15:26 +0200

    Added gateways config

--- config/mena-payment-gateways-for-laravel.php
+++ config/mena-payment-gateways-for-laravel.php
@@ -2,5 +2,44 @@
 
 // config for Saeedeldeeb/PaymentGateway
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Default Payment Gateway
+    |--------------------------------------------------------------------------
+    |
+    | This option controls the default payment gateway that will be used to process
+    | payments for your application. By default, the urway gateway is
+    | used; however, you remain free to modify this option if you wish.
+    |
+    | Supported: "urway", "clickPay"
+    |
+    */
 
+    'gateway' => env('DEFAULT_GATEWAY', 'clickpay'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | UrWay Option
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the configuration options that should be used when
+    | payments are processed using the UrWay gateway. This will allow you
+    | to control the service to complete payments.
+    |
+    */
+    'urway' => [
+        'auth' => [
+            'terminal_id' => env('URWAY_TERMINAL_ID'),
+            'password' => env('URWAY_PASSWORD'),
+            'merchant_key' => env('URWAY_MERCHANT_KEY'),
+            'base_bath' => env('URWAY_BASE')
+        ],
+        'currency' => env('URWAY_CURRENCY')
+    ],
+    'clickPay' => [
+        'base_bath' => env('CLICK_PAY_BASE_URL'),
+        'server_key' => env('CLICK_PAY_SERVER_KEY'),
+        'profile_id' => env('CLICK_PAY_PROFILE_ID'),
+    ],
+    'default_currency' => env('DEFAULT_CURRENCY', "SAR")
 ];
